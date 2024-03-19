4fe6660cc070f96bdac6590075fff20eaa2c40f9
Ricardo Fernandez <rckfr.fr@hotmail.com>
2024-03-19 13:13:00 -0300

    refactor night mode

--- utils.js
+++ utils.js
@@ -75,4 +75,51 @@ const setUserList = (users, cantidad) => {
   })
 }
 
-export { setDisponible, setNoDisponible, sendForm, setCantidad, setUserList }
+const toggleNightMode = () => {
+  var cssMode = localStorage.getItem("cssMode")
+    if (cssMode == "night") {
+      const link = document.createElement("link")
+      link.rel = "stylesheet"
+      link.type = "text/css"
+      link.href = "/templates/dark.css"
+      link.id = "night"
+      document.head.appendChild(link)
+      localStorage.setItem("cssMode", "night")
+      document.querySelector("#css-mode").querySelector("img").src =
+        "https://img.icons8.com/?size=50&id=648&format=png"
+    }
+
+    document.querySelector("#css-mode").addEventListener("click", (e) => {
+      var cssMode = localStorage.getItem("cssMode")
+      console.log(cssMode)
+      if (cssMode !== "night") {
+        const link = document.createElement("link")
+        link.rel = "stylesheet"
+        link.type = "text/css"
+        link.href = "/templates/dark.css"
+        link.id = "night"
+        document.head.appendChild(link)
+        localStorage.setItem("cssMode", "night")
+        document.querySelector("#css-mode").querySelector("img").src =
+          "https://img.icons8.com/?size=50&id=648&format=png"
+      } else {
+        var link = document.getElementById("night")
+        if (link) {
+          link.parentNode.removeChild(link)
+        }
+        localStorage.setItem("cssMode", "day")
+        document.querySelector("#css-mode").querySelector("img").src =
+          "https://img.icons8.com/?size=50&id=45474&format=png"
+      }
+    })
+}
+
+
+export {
+  setDisponible,
+  setNoDisponible,
+  sendForm,
+  setCantidad,
+  setUserList,
+  toggleNightMode,
+}
